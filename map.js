05506c3b538d561292e8bdbd27820aa15e1db919
Keenan McKibben <40180906+Xx-DemonZlAyrKeenan-xX@users.noreply.github.com>
2018-06-26 13:43:30 -0400

    Add files via upload

---
+++ map.js
@@ -0,0 +1,31 @@
+const map = [
+    "    WWWWWWW         ",
+    "    W     W         ",
+    "    W     W         ",
+    "    WB  B W         ",
+    "  WWW  B WW         ",
+    "  W  B B W          ",
+    "WWW W WW W     WWWWW",
+    "W   W WW WWWWWWW OOW",
+    "W B  B          OOOW",
+    "WWWWW WWW WSWWWW OOW",
+    "    W     WWW  WWWWW",
+    "    WWWWWWW         ",
+];
+
+/* Alternate map
+
+const map = [
+   "    WWWWW          ",
+   "    W   W          ",
+   "    WB  W          ",
+   "  WWW  BWW         ",
+   "  W  B B W         ",
+   "WWW W WW W   WWWWWW",
+   "W   W WW WWWWW  OOW",
+   "W B  B          OOW",
+   "WWWWW WWW WSWW  OOW",
+   "    W     WWWWWWWWW",
+   "    WWWWWWW        "
+]
+ */
