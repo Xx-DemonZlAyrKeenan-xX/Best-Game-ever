363e7fdfe62168a4ca5ec0155168d6a2c989cf26
Keenan McKibben <40180906+Xx-DemonZlAyrKeenan-xX@users.noreply.github.com>
2018-06-27 14:17:40 -0400

    Update map2.js

--- map2.js
+++ map2.js
@@ -1,1 +1,12 @@
 
+const map = [
+    "      WWWWW      ",
+    "     WW   WW     ",
+    "    WW  B  WW    ",
+    "    W   O   W    ",
+    "    W BOSOB W    ",
+    "    W   O   W    ",
+    "    WW  B  WW    ",
+    "     WW   WW     ",
+    "      WWWWW      ",
+];  
