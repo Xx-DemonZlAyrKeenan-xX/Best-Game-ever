b854c17e6e3c8a396106a136cccd687c20dc2ccf
Keenan McKibben <40180906+Xx-DemonZlAyrKeenan-xX@users.noreply.github.com>
2018-06-28 11:10:21 -0700

    Add files via upload

---
+++ map1.js
@@ -0,0 +1,12 @@
+
+const map1= [
+    "      WWWWW      ",
+    "     WW   WW     ",
+    "    WW  B  WW    ",
+    "    W   O   W    ",
+    "    W BOSOB W    ",
+    "    W   O   W    ",
+    "    WW  B  WW    ",
+    "     WW   WW     ",
+    "      WWWWW      ",
+];  
