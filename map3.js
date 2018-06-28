fd2a191cf97f55eab66e72fa10b1eba7cc89700e
Keenan McKibben <40180906+Xx-DemonZlAyrKeenan-xX@users.noreply.github.com>
2018-06-28 11:04:14 -0700

    Add files via upload

---
+++ map3.js
@@ -0,0 +1,11 @@
+
+const map3= [
+   "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW ",
+   "WS    W B            OWOWOWWOO      W ",
+   "WWWWW W WWWWWWWWWW B WW W WWWWWWWB BW ",
+   "    W   W        W      W W     W W WW",
+   "    WWWWW        WWWWW  WBWWWWWWW W  W",
+   "                     WW              W",
+   "                      WWWWWWWWWWW  WWW",
+   "                                WWWW  ",
+]; 
